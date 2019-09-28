abf7d5dd55d54aacd0308b35055c04cc7252c7ff
Brandy Mello <brandyamello@gmail.com>
2019-09-28 14:49:42 -0600

    Clean up by removing apicall, cannot figure out how to POST to this

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// import postMessage from './apicalls';
-
 const http = require('http');
 const url = require('url');
 const server = http.createServer();
@@ -13,12 +11,6 @@ let messages = [
   { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
   { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
 ];
-
-// postMessage({
-//   'id': 4,
-//   'user': 'Brandy',
-//   'message': 'What?'
-// });
 
 const getAllMessages = (response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain', 'Accept': 'application/json' });
